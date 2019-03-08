08c14236984a155c9b0cda8708fcd5b1715da367
Christian Bundy <christianbundy@fraction.io>
2019-03-07 18:37:17 -0800

    Fix relative path in build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 
-browserify -t sheetify src/client.js | indexhtmlify > build/index.html
+set -e
+
+PKGDIR="$(cd "$(dirname "$0")"; pwd -P )/../"
+PATH="$(cd "$PKGDIR" && npm bin):$PATH"
+
+cd $PKGDIR
+browserify -t sheetify "$PKGDIR/src/client.js" | indexhtmlify > "$PKGDIR/build/index.html"
